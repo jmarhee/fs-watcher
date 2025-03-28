3ff9f3c6497a43b4acc6fd73d2c57bc4f0c62c47
Joseph D. Marhee <jmarhee@interiorae.com>
2025-03-28 13:44:00 -0500

    Updates script to be generalized
    Signed-off-by: Joseph D. Marhee <jmarhee@interiorae.com>

--- make-release.sh
+++ make-release.sh
@@ -3,12 +3,14 @@
 TAG=$TAG
 TAG_COMMIT=$(git rev-parse --short ${TAG})
 
+REPO_NAME=$(git remote -v | grep push | awk '{print $2}' | sed -e 's|git@github.com:||g' -e 's|.git||g')
+
 AUTH_TOKEN=$GITHUB_TOKEN
 
 curl -X POST \
   -H "Authorization: token $GITHUB_TOKEN" \
   -H "Accept: application/vnd.github.v3+json" \
-  "https://api.github.com/repos/jmarhee/fs-watcher/releases" \
+  "https://api.github.com/repos/$REPO_NAME/releases" \
   -d "{
     \"tag_name\": \"$TAG\",
     \"name\": \"Release $TAG\",
